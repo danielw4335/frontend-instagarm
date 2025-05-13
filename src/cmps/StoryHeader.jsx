6245ecf2432ea47d3affe5e90aa86d5b6aeae65b
danielw4335 <daniel.wallache@gmail.com>
2025-05-14 00:22:53 +0300

    details header css

--- src/cmps/StoryHeader.jsx
+++ src/cmps/StoryHeader.jsx
@@ -3,28 +3,30 @@ import { getTimeFormat } from '../services/util.service'
 import { useModal } from '../customHooks/ModalContext.jsx'
 
 export function StoryHeader({ from, _id, user, createdAt }) {
-console.log(' StoryHeader from:', from)
+    console.log(' StoryHeader from:', from)
 
     const { open } = useModal()
     const { username, imgUrl } = user
     const timeAgo = getTimeFormat(createdAt)
     return (
         <section className="storyHeader">
-            <button onClick={() => open(_id)}>***</button>
-            {from === 'index' && (
-                <div className="storyHeader-index">
-                    <img className="user-img" src={imgUrl} alt="user" />
-                    <span className="username">{user.username}</span>
-                    <span className="created-time">•</span>
-                    <span className="created-time">{timeAgo}</span>
-                </div>
-            )}
-            {from === 'details' && (
-                <div className="storyHeader-details">
-                    <img className="user-img" src={imgUrl} alt="user" />
-                    <span className="username">{user.username}</span>
-                </div>
-            )}
+            <div className="storyHeader-container">
+                <button className='clear-button btn-options' onClick={() => open(_id)}>⋯</button>
+                {from === 'index' && (
+                    <div className="storyHeader-index">
+                        <img className="user-img" src={imgUrl} alt="user" />
+                        <span className="username">{user.username}</span>
+                        <span className="created-time">•</span>
+                        <span className="created-time">{timeAgo}</span>
+                    </div>
+                )}
+                {from === 'details' && (
+                    <div className="storyHeader-details">
+                        <img className="user-img" src={imgUrl} alt="user" />
+                        <span className="username">{user.username}</span>
+                    </div>
+                )}
+            </div>
         </section>
     )
 }
