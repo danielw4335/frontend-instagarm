63bea5a02fdc6c4f46bd6dcd99e37240af2acb4f
danielw4335 <daniel.wallache@gmail.com>
2025-05-15 20:50:30 +0300

    Small sideNav done

--- src/cmps/SideNav.jsx
+++ src/cmps/SideNav.jsx
@@ -33,7 +33,7 @@ export function SideNav() {
                 <LogoInstagramWordmark className="nav-label logo-wordmark" />
             </div>
             <div className="logo-icon" onClick={() => navigate('/')}>
-                <LogoInstagram />
+                <LogoInstagram className="logo-icon-svg" />
             </div>
 
             <div className="nav-section">
